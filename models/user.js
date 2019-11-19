8820bdb6fa5f1e2628a4c5813683ed6a92d56109
Glitch (boggy-gruyere) <none>
2019-11-19 20:12:19 +0000

    🚝🎎 Checkpoint ./models/user.js:987705/63

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  username: String
+  username: String,
+  exercises: [{ description: String, }]
 });
 
 module.exports = mongoose.model('User', userSchema);
